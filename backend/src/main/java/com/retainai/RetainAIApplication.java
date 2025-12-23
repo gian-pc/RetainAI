465e472734e754c5845f251bba93fbaa4b85eb9d
Juan Francisco <juanfranciscojesus.hernandez@gmail.com>
2025-12-23 02:38:45 -0600

    Módulo Backend listo para QA, SpringBoot y configuración inicial.

---
+++ backend/src/main/java/com/retainai/RetainAIApplication.java
@@ -0,0 +1,12 @@
+package com.retainai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetainAIApplication {
+    public static void main(String[] args){
+        SpringApplication.run(RetainAIApplication.class, args);
+    }
+}
+    
